bc5db137575016bd35d9ca7d424a7635e4e85a8e
Matus Petrofcik - behemoth <petrofcikmatus@gmail.com>
2017-07-04 10:22:12 +0200

    update, do not use html entities for czech characters in ckeditor

--- public/adminaut/themes/adminlte/plugins/ckeditor/config.js
+++ public/adminaut/themes/adminlte/plugins/ckeditor/config.js
@@ -37,4 +37,7 @@ CKEDITOR.editorConfig = function (config) {
 
     // Default skin
     config.skin = 'moono-lisa';
+
+    // Whether to use HTML entities in the editor output.
+    config.entities = false;
 };
