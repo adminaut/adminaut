b8e28f6356142491cdf92cbe9b7b0744b03e95ba
Matus Petrofcik - behemoth <petrofcikmatus@gmail.com>
2017-10-19 16:35:33 +0200

    Views clean-up.

--- config/module.config.php
+++ config/module.config.php
@@ -675,8 +675,14 @@ return [
 
     'view_manager' => [
         'template_map' => [
+            // layout
             'layout/admin' => __DIR__ . '/../view/layout/layout-admin.phtml',
             'layout/admin-blank' => __DIR__ . '/../view/layout/layout-admin-blank.phtml',
+
+            // partial
+            'adminaut/partial/breadcrumbs' => __DIR__ . '/../view/partial/breadcrumbs.phtml',
+            'adminaut/partial/messages' => __DIR__ . '/../view/partial/messages.phtml',
+            'adminaut/partial/tabs' => __DIR__ . '/../view/partial/tabs.phtml',
         ],
         'template_path_stack' => [
             'Adminaut' => __DIR__ . '/../view',
