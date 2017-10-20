445a9694cdf2cb00c4d034223f502ff20fc3a5a0
Matus Petrofcik - behemoth <petrofcikmatus@gmail.com>
2017-10-20 10:58:38 +0200

    Updated Radio data type so it's value can be in the list.

--- src/Datatype/Radio.php
+++ src/Datatype/Radio.php
@@ -8,5 +8,31 @@ namespace Adminaut\Datatype;
  */
 class Radio extends \Zend\Form\Element\Radio
 {
+    use Datatype {
+        setOptions as datatypeSetOptions;
+    }
 
+    /**
+     * @param array|\Traversable $options
+     * @return $this
+     */
+    public function setOptions($options)
+    {
+        parent::setOptions($options);
+        $this->datatypeSetOptions($options);
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getListedValue()
+    {
+        if (isset($this->getValueOptions()[$this->getValue()])) {
+            return $this->getValueOptions()[$this->getValue()];
+        }
+
+        return 'Unknown';
+    }
 }
